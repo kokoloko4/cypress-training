a7000515f431cf9e9bef15db9c1aed9dfb27949e
kokoloko4 <anfecoquin@hotmail.com>
2023-04-18 11:56:07 -0500

    setup CI and some eslint fixes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // e2e testing node events setup code
-    },
-  },
+      config.defaultCommandTimeout = 10000;
+      config.responseTimeout = 20000;
+      config.pageLoadTimeout = 60000;
+      return config;
+    }
+  }
 });
