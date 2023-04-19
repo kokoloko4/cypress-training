769b30c3b475dd3ab8c5de159792ad3f399305d1
Andrés Cocunubo <anfecoquin@hotmail.com>
2023-04-19 10:52:45 -0500

    add mochawesome reporter (#12)

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,17 @@ export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   chromeWebSecurity: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quiet: true,
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // e2e testing node events setup code
